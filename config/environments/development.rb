abf5d99aca3925952316f2d4afcfd62afc682b9a
guzmanweb <carlos@guzmanweb.com>
2015-09-02 14:31:00 +0000

    Config files refactor

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,10 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.asset_host = "http://localhost:3000"
+
+  # Deliver emails to a development mailbox at /letter_opener
+  config.action_mailer.delivery_method = :letter_opener
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -40,6 +43,16 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # bullet config
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+    if ENV['BULLET']
+      Bullet.rails_logger = true
+      Bullet.add_footer = true
+    end
+  end
 
   config.web_console.whitelisted_ips = ENV['TRUSTED_IP'] if ENV['TRUSTED_IP']
 end
