f126beb2557be61bb840b0bfb32a125e598ea26b
alle <agileontheweb@gmail.com>
2015-09-16 19:57:46 +0200

    recreate all

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,5 @@
 //Preload
+ 
 $(window).load(function() {
   // start up after 2sec no matter what
   window.setTimeout(function(){
@@ -6,24 +7,44 @@ $(window).load(function() {
   }, 1000);
   scrollalert();
 });
- 
+  
 $(function () {
   //headroom
   headroom();
+  
+  //remove hidden class section if XS
+  if (window.innerHeight > window.innerWidth) { 
+    } else {
+      scrollPage();
+    }
+
+  //Auto close navbar XS mode on click outside
+  $('body').bind('click', function(e) {
+    if(jQuery(e.target).closest('.navbar').length == 0) {
+      console.log("fuori");
+      var opened = jQuery('.navbar-collapse').hasClass('collapse in');
+      if ( opened === true ) {
+          jQuery('.navbar-collapse').collapse('hide');
+      }
+    }
+  });
+    
+  //Auto close navbar XS mode on click link navbar
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+
+  $(".nav a").on("click", function(){
+    $(".nav").find(".active").removeClass("active");
+    $(this).parent().addClass("active");
+  });
 
   //tooltips 
   $('[data-toggle="tooltip"]').tooltip();
 
-  //on scroll, show section 
-  $('section').addClass("hidden-section").viewportChecker({
-    classToAdd: 'visible-section animated fadeIn', // Class to add to the elements when they are visible
-    offset: 200    
-  });
-
-  
-  //scroll to section on click button menu
+  //scroll to section on click button menu  
   $('a[href*=#]:not([href=#])').click(function() {
-    $('.navbar-toggle').click();
+   // $('.navbar-toggle').click();  
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -48,8 +69,8 @@ function scrollalert(){
 function headroom(){ 
    var myElement = document.querySelector("nav");
     var headroom = new Headroom(myElement, {
-      "offset": 205,
-      "tolerance": 25,
+      "offset": 40,
+      "tolerance": 5,
       "classes": {
         "initial": "animated",
         "pinned": "headroom-pinned",
@@ -58,3 +79,11 @@ function headroom(){
     });
     headroom.init();
 }
+
+//on scroll, show section 
+function scrollPage(){
+    $('section').addClass("hidden-section").viewportChecker({
+      classToAdd: 'visible-section animated fadeIn',// Class to add to the elements when they are visible
+      offset: 200    
+    });
+  }
