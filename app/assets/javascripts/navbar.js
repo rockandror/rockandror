ff5b598a27f909e69426f3e58a0e14bf6b3c8872
alle <agileontheweb@gmail.com>
2015-11-21 20:46:55 +0100

    add nav active section

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,23 +1,33 @@
 $(function () {
-  // Changing menu dependent on section
-  $(window).scroll(function() {
+  $(document).on("scroll", onScroll);
+  
+  function onScroll(event){
+
     var windowTop = Math.max($('body').scrollTop(), $('html').scrollTop());  
-      $('.pageSection').each(function (index) {
-        if (windowTop > ($(this).position().top - ($(window).height() / 1.2))){
-            $('.pageSection:eq(' + index + ')').addClass('addopacity');
-            console.log($('.pageSection:eq(' + index + ')').attr('id'));
-          }
-          if (windowTop >= 300) {
-           $('.contain-to-grid').css( 'background', 'black' );        
-          }
-      });
-  }).scroll();
-
+    $('.pageSection').each(function (index) {
+      if (windowTop > ($(this).position().top - ($(window).height() / 1.2))){
+        $('.pageSection:eq(' + index + ')').addClass('addopacity');
+      }
+      if (windowTop >= 300) {
+       $('.contain-to-grid').css( 'background', 'black' );        
+     }
+   });
+    var scrollPos = $(document).scrollTop();
+    $('.top-bar-section a').each(function () {
+      var currLink = $(this);
+      var refElement = $(currLink.attr("href"));
+      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos ) {
+        $('.top-bar-section li').removeClass("active");
+        currLink.parent().addClass("active");
+      }
+      else{
+        currLink.removeClass("active");
+      }
+    });
+  }
   $('.top-bar-section ul.right li a').click(function() {
-    console.log("s");
-       $('.top-bar').removeClass('expanded');
-     });
-
+    $('.top-bar').removeClass('expanded');
+  });
   $('body').bind('click', function(e) {
     if(jQuery(e.target).closest('.navbar').length == 0) {
       var opened = jQuery('.navbar-collapse').hasClass('collapse in');
