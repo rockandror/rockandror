777c00de44c7c782eb49533fd067beb12f72cf88
Senén Rodero Rodríguez <senenrodero@gmail.com>
2016-02-16 21:26:59 +0100

    Some fixes

--- spec/features/contact_spec.rb
+++ spec/features/contact_spec.rb
@@ -7,7 +7,7 @@ feature 'contact request' do
     fill_in 'Nombre', with: "Nombre"
     fill_in 'Email', with: "email@example.es"
     fill_in 'Mensaje', with: "Solicitud de contacto"  
-    click_on 'Enviar'
+    click_on 'Contacta ahora'
     expect(page).to have_content "Gracias por contactar con nosotros. Le atenderemos en cuanto sea posible."    
   end
 
@@ -16,7 +16,7 @@ feature 'contact request' do
     fill_in 'Nombre', with: ""
     fill_in 'Email', with: "email@example.es"
     fill_in 'Mensaje', with: "Solicitud de contacto"  
-    click_on 'Enviar'
+    click_on 'Contacta ahora'
     expect(page).to have_content "no puede estar en blanco"    
   end  
 
