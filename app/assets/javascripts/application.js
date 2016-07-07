2bbc68632c619f578db58ecf59ea09396b8b87db
alle <agileontheweb@gmail.com>
2016-07-07 17:58:10 +0200

    Add nprogress gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require nprogress
+//= require nprogress-turbolinks
+//= require nprogress-ajax
 //= require foundation
 //= require preload
 //= require navbar
