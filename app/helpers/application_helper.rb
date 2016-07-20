fb52b41bf65e222d25d74b36fee6f562f1d41cc4
Sebastia <sebastia.roig@gmail.com>
2016-07-20 13:30:38 +0200

    add active helper to navbar links [#27]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def active?(action, parameter)
+    klass = ""
+    if parameter.present?
+      if action == parameter
+        klass = "active" 
+      elsif action == "portfolio" && (parameter == "eatbooking" || parameter == "webdings" || parameter == "oasiscatamaran")
+         klass = "active"
+      else
+        klass = ""
+      end
+    end
+    klass
+  end
 end
