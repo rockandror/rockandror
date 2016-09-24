ccbe3dbacaa6d89f8c063c8c30082c148cd9bd41
Alessandro Cuoghi <agileontheweb@gmail.com>
2016-09-24 15:49:00 +0000

    Fix logo mediaquery

--- app/assets/javascripts/responsive_animation.js
+++ app/assets/javascripts/responsive_animation.js
@@ -188,14 +188,14 @@ function setIntroAnimation() {
 
           $(".owl-carousel").owlCarousel({
 
-          // itemsCustom : false,
-          // itemsDesktop : [1199,4],
-          // itemsDesktopSmall : [980,3],
-          // itemsTablet: [768,2],
-          // itemsTabletSmall: false,
-          // itemsMobile : [479,1],
-          // singleItem : false,
-          // itemsScaleUp : false,
+          itemsCustom : false,
+          itemsDesktop : [1199,4],
+          itemsDesktopSmall : [980,3],
+          itemsTablet: [768,2],
+          itemsTabletSmall: false,
+          itemsMobile : [479,1],
+          singleItem : false,
+          itemsScaleUp : false,
        
           // //Basic Speeds
           // slideSpeed : 200,
