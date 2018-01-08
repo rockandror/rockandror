d9c34c17e8aab94a7ad8da3bc8996c81b0fc397b
Alessandro Cuoghi <agileontheweb@gmail.com>
2018-01-08 15:04:23 +0100

    Added some image in boatjump page. Create helper for meta title, description, keywords.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,18 @@
 module PagesHelper
 
+  def title(text)
+    content_for :title, text
+  end
+
+  def meta_tag(tag, text)
+    content_for :"meta_#{tag}", text
+  end
+
+  def yield_meta_tag(tag, default_text='')
+    content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
+  end
+
+
   def contact_now
     if params[:action] == "team"
       'Te ha gustado nuestro equipo y nuestra filosofia?'
