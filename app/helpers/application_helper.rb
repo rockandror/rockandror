85a1d20aa6842bcfb572e44a051af210b50908b6
Alessandro Cuoghi <agileontheweb@gmail.com>
2018-02-05 16:54:19 +0100

    Footer: Added link to phone. General: Changed some font size. Home XS: Changed color style contact button.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     klass = ""
     if parameter.present?
       if action == parameter
-        klass = "active disable" 
+        klass = "active disable"
       elsif action == "portfolio" && (parameter == "eatbooking" || parameter == "webdings" || parameter == "oasiscatamaran")
          klass = "active"
       else
@@ -11,5 +11,10 @@ module ApplicationHelper
       end
     end
     klass
+  end
+
+  def tel_to(text)
+    groups = text.to_s.scan(/(?:^\+)?\d+/)
+    link_to text, "tel:#{groups.join '-'}"
   end
 end
