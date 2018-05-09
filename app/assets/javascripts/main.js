867ed6dc58475220586be1a9f5c1b8eb1c6a348f
taitus <sebastia.roig@gmail.com>
2018-05-09 16:25:06 +0200

    Add new step with modules summary. Change visualization from responsive version

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -17,19 +17,30 @@ function init(){
 
 }
 
-function budgetAmountWidth(){
-  var budget_ammount = $('form.new_budget .wizard .steps li a').width();
-  $('#budget_ammount').width(budget_ammount);
+// function budgetAmountWidth(){
+//   var budget_ammount = $('form.new_budget .wizard .steps li a').width();
+//   $('#budget_ammount').width(budget_ammount);
+// }
+
+function isMobile() {
+  try{ document.createEvent("TouchEvent"); return true; }
+  catch(e){ return false; }
 }
 
 function initBudgetSteps(){
   var wizard = $("#new_budget div");
+  if (isMobile()) {
+    var orientation = "horizontal"
+  } else {
+    var orientation = "vertical"
+  }
+
   wizard.steps({
       headerTag: "h3",
       bodyTag: "section",
       transitionEffect: "none",
       titleTemplate: "#title#",
-      stepsOrientation: "vertical",
+      stepsOrientation: orientation,
       onFinished: function (event, currentIndex)
       {
         wizard.closest("form").submit();
@@ -40,6 +51,7 @@ function initBudgetSteps(){
   });
 
   if (($("#new_budget").data("errored") == true) || ($("#new_budget").data("received-params") == true)) {
+    wizard.steps("next");
     wizard.steps("next");
     wizard.steps("next");
     wizard.steps("next");
@@ -88,61 +100,73 @@ function updatePrice(){
     if ($("#budget_module_1")[0].checked == true) {
       $("#budget_ammount").val(parseInt($("#budget_ammount").val()) + 1000 + ' €')
       $("#checked-1").html("√")
+      $("#title_mod_1").html("Preparación de servidores")
     } else {
       $("#budget_ammount").val(parseInt($("#budget_ammount").val()) - 1000 + ' €')
       $("#checked-1").html("")
+      $("#title_mod_1").html("")
     }
   });
   $("#budget_module_2").click(function() {
     if ($("#budget_module_2")[0].checked == true) {
       $("#budget_ammount").val(parseInt($("#budget_ammount").val()) + 1000 + ' €')
       $("#checked-2").html("√")
+      $("#title_mod_2").html("Configuración e Instalación")
     } else {
       $("#budget_ammount").val(parseInt($("#budget_ammount").val()) - 1000 + ' €')
       $("#checked-2").html("")
+      $("#title_mod_2").html("")
     }
   });
   $("#budget_module_3").click(function() {
     if ($("#budget_module_3")[0].checked == true) {
       $("#budget_ammount").val(parseInt($("#budget_ammount").val()) + 500 + ' €')
       $("#checked-3").html("√")
+      $("#title_mod_3").html("Adaptación visual")
     } else {
       $("#budget_ammount").val(parseInt($("#budget_ammount").val()) - 500 + ' €')
       $("#checked-3").html("")
+      $("#title_mod_3").html("")
     }
   });
   $("#budget_module_4").click(function() {
     if ($("#budget_module_4")[0].checked == true) {
       $("#budget_ammount").val(parseInt($("#budget_ammount").val()) + 2000 + ' €')
       $("#checked-4").html("√")
+      $("#title_mod_4").html("Integración Censo")
     } else {
       $("#budget_ammount").val(parseInt($("#budget_ammount").val()) - 2000 + ' €')
       $("#checked-4").html("")
+      $("#title_mod_4").html("")
     }
   });
   $("#budget_module_5").click(function() {
     if ($("#budget_module_5")[0].checked == true) {
       $("#budget_ammount").val(parseInt($("#budget_ammount").val()) + 3200 + ' €')
       $("#checked-5").html("√")
+      $("#title_mod_5").html("Personalización de procesos")
     } else {
       $("#budget_ammount").val(parseInt($("#budget_ammount").val()) - 3200 + ' €')
       $("#checked-5").html("")
+      $("#title_mod_5").html("")
     }
   });
   $("#budget_module_6").click(function() {
     if ($("#budget_module_6")[0].checked == true) {
       $("#budget_ammount").val(parseInt($("#budget_ammount").val()) + 7200 + ' €')
       $("#checked-6").html("√")
+      $("#title_mod_6").html("Mantenimiento y monitorización")
     } else {
       $("#budget_ammount").val(parseInt($("#budget_ammount").val()) - 7200 + ' €')
       $("#checked-6").html("")
+      $("#title_mod_6").html("")
     }
   });
 }
 
 $(window).on('resize', function(){
   resizeDiv();
-  budgetAmountWidth();
+  // budgetAmountWidth();
 });
 
 var ready;
