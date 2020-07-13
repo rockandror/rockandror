ce35b85e7e31c7f401e22cef76d3d732f602e1c5
Senén Rodero Rodríguez <senenrodero@gmail.com>
2020-07-13 14:24:21 +0200

    Add language selector
    * Add items to responsive and desktop menus
    * Configure fallbacks explicitly
    * Store locale in session and use it if not defined
    * Add langiages translations
    * Make responsive top navbar icon a little bit smaller

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
     port: 587,
     authentication: 'plain',
     enable_starttls_auto: true
-  }  
+  }
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -77,10 +77,6 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
