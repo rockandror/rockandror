db959eb1230fba042fc785a435457773ba11cd83
Senén Rodero Rodríguez <senenrodero@gmail.com>
2020-07-30 17:28:11 +0200

    Add support for dynamic meta tags
    Now we can add custom meta title, description and keywords
    entries for every page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,16 @@ module ApplicationHelper
     end
     klass
   end
+
+  def meta_title
+    content_for?(:meta_title) ? content_for(:meta_title) : t("meta.title")
+  end
+
+  def meta_description
+    content_for?(:meta_description) ? content_for(:meta_description) : t("meta.description")
+  end
+
+  def meta_keywords
+    content_for?(:meta_keywords) ? content_for(:meta_keywords) : t("meta.keywords")
+  end
 end
