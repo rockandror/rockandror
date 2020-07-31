c3f420297a007e49cab5564539c98f3abbcedcc1
Senén Rodero Rodríguez <senenrodero@gmail.com>
2020-07-31 13:33:32 +0200

    Add helper to improve how we handle menu elements activation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,4 @@
 module ApplicationHelper
-  def active?(action, parameter)
-    klass = ""
-    if parameter.present?
-      if action == parameter
-        klass = "active disable" 
-      elsif action == "portfolio" && (parameter == "eatbooking" || parameter == "webdings" || parameter == "oasiscatamaran")
-         klass = "active"
-      else
-        klass = ""
-      end
-    end
-    klass
-  end
-
   def meta_title
     content_for?(:meta_title) ? content_for(:meta_title) : t("meta.title")
   end
